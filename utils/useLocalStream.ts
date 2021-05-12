14c39e6f28a042b3b2f5b1dea6f2de11bd06b973
Ozan <ozanbolel@outlook.com>
2021-05-12 23:31:29 +0300

    no noisesuppressor

--- utils/useLocalStream.ts
+++ utils/useLocalStream.ts
@@ -24,8 +24,7 @@ export const useLocalStream = () => {
         .ready()
         .start(destination);
 
-      streamModule.module("noisegate").param("level", 1);
-      streamModule.module("noisesuppressor").param("threshold", 0.1);
+      streamModule.module("noisegate").param("level", 0.5);
 
       dispatch({ type: "SET_LOCAL_STREAM", payload: destination.stream });
       resolve(destination.stream);
